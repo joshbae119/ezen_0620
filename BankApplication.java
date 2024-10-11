cbd75d0f5f3cdc8a061ee89b71202ded6de629fc
joshbae119 <mclceo119@gmail.com>
2024-10-11 12:25:49 +0900

    V1

--- BankApplication.java
+++ BankApplication.java
@@ -2,33 +2,29 @@ package exam0620;
 
 import java.util.Scanner;
 
-
 public class BankApplication {
 
-	private static Account[] accountArray = new Account[100];
+	private static Account[] accounts = new Account[100];
 	private static Scanner scanner = new Scanner(System.in);
 
 	public static void main(String[] args) {
-		boolean run = true;
-		while(run) {
-		System.out.println("----------------------------------------------------------");
-		System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
-		System.out.println("----------------------------------------------------------");
-		System.out.print("선택> ");
-		int selectNo = Integer.parseInt(scanner.nextLine());
-			if(selectNo == 1) {
-				createAccount();
-			} else if(selectNo == 2) {
-				accountList();
-			} else if(selectNo == 3) {
-				deposit();
-			} else if(selectNo == 4) {
-				withdraw();
-			} else if(selectNo == 5) {
-				run = false;
+		boolean isRunning = true;
+		while(isRunning) {
+			System.out.println("----------------------------------------------------------");
+			System.out.println("1.계좌생성 | 2.계좌목록 | 3.예금 | 4.출금 | 5.종료");
+			System.out.println("----------------------------------------------------------");
+			System.out.print("선택> ");
+			int choice = Integer.parseInt(scanner.nextLine());
+			
+			switch(choice) {
+				case 1: createAccount(); break;
+				case 2: showAccountList(); break;
+				case 3: deposit(); break;
+				case 4: withdraw(); break;
+				case 5: isRunning = false; break;
 			}
 		}
-		System.out.println("프로그램 종료");
+		System.out.println("프로그램종료");
 	}
 
 		private static void createAccount() {
@@ -37,15 +33,15 @@ public class BankApplication {
 		System.out.println("--------------");
 		System.out.print("계좌번호: ");
 		String ano = scanner.nextLine();
-		System.out.print("계좌주: ");
+		System.out.print("예금주: ");
 		String owner = scanner.nextLine();
-		System.out.print("초기입금액: ");
+		System.out.print("초기입금: ");
 		int balance = Integer.parseInt(scanner.nextLine());
 		Account newAccount = new Account(ano, owner, balance);
-			for(int i=0; i<accountArray.length; i++) {
-				if(accountArray[i] == null) {
-					accountArray[i] = newAccount;
-					System.out.println("결과: 계좌가 생성되었습니다.");
+			for(int i=0; i<accounts.length; i++) {
+				if(accounts[i] == null) {
+					accounts[i] = newAccount;
+					System.out.println("결과: 계좌 생성 성공.");
 					break;
 				}
 			}
@@ -55,8 +51,8 @@ public class BankApplication {
 		System.out.println("--------------");
 		System.out.println("계좌목록");
 		System.out.println("--------------");
-			for(int i=0; i<accountArray.length; i++) {
-				Account account = accountArray[i];
+			for(int i=0; i<accounts.length; i++) {
+				Account account = accounts[i];
 				if(account != null) {
 					System.out.print(account.getAno());
 					System.out.print(" ");
@@ -78,11 +74,11 @@ public class BankApplication {
 			int money = Integer.parseInt(scanner.nextLine());
 			Account account = findAccount(ano);
 			if(account == null) {
-				System.out.println("결과: 계좌가 없습니다.");
+				System.out.println("결과: 해당 계좌가 없습니다.");
 				return;
 			}
 			account.setBalance(account.getBalance() + money);
-			System.out.println("결과: 예금이 성공되었습니다.");
+			System.out.println("결과: 예금 성공!");
 		}
 		//출금하기
 		private static void withdraw() {
@@ -96,20 +92,20 @@ public class BankApplication {
 		
 		Account account = findAccount(ano);
 			if(account == null) {
-				System.out.println("결과: 계좌가 없습니다.");
+				System.out.println("결과: 계좌가 존재하지 않습니다. ");
 				return;
 			}
 			account.setBalance(account.getBalance() - money);
-			System.out.println("결과: 출금이 성공되었습니다.");
+			System.out.println("결과: 출금 성공!");
 		}
 		//Account 배열에서 ano와 동일한 Account 객체 찾기
 		private static Account findAccount(String ano) {
 		Account account = null;
-			for(int i=0; i<accountArray.length; i++) {
-				if(accountArray[i] != null) {
-					String dbAno = accountArray[i].getAno();
+			for(int i=0; i<accounts.length; i++) {
+				if(accounts[i] != null) {
+					String dbAno = accounts[i].getAno();
 					if(dbAno.equals(ano)) {
-						account = accountArray[i];
+						account = accounts[i];
 						break;
 					}
 				}
